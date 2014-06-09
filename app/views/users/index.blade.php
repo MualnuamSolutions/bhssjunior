ec1dec1ae3853a543b23db4bf525fa47838a1f45
Alan Pachuau <alanpachuau@gmail.com>
2014-06-09 20:50:21 +0530

    update users list view

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,31 +1,45 @@
 @extends('layout')
 
 @section('content')
+   <ul class="breadcrumbs">
+      <li><a href="{{ route('home') }}">Home</a></li>
+      <li class="current">Users</li>
+   </ul>
 
-   <h4><i class="fi-page-multiple"></i> Users</h4>
-   <hr>
-   <table>
-      <thead>
-         <tr>
-            <th>Email</th>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>Role</th>
-         </tr>
-      </thead>
-      <tbody>
-         @foreach($users as $i => $user)
-         <tr>
-            <td>{{ $user->email }}</td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->phone }}</td>
-            <td>{{ $user->phone }}</td>
-         </tr>
-         @endforeach
-      </tbody>
-   </table>
+   <div class="panel">
+      <h5><i class="fi-page-multiple"></i> Users</h5>
+      <hr>
 
-   {{ $users->links() }}
+      <table class="small-12">
+         <thead>
+            <tr>
+               <th>#</th>
+               <th>Email</th>
+               <th>Name</th>
+               <th>Phone</th>
+               <th>Role</th>
+            </tr>
+         </thead>
+         <tbody>
+            @foreach($users as $key => $user)
+            <tr>
+               <td>{{ $key+$users->getFrom() }}</td>
+               <td>{{ $user->email }}</td>
+               <td>{{ $user->name }}</td>
+               <td>{{ $user->phone }}</td>
+               <td>{{ $user->phone }}</td>
+            </tr>
+            @endforeach
+         </tbody>
+      </table>
 
+      {{ $users->links() }}
+      <hr>
+      <div class="row">
+         <div class="small-12 columns text-right">
+            <a href="{{ route('users.create') }}" class="button tiny">Create New User</a>
+         </div>
+      </div>
+   </div>
 
 @stop
