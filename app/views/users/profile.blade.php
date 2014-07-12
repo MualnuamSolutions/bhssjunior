0ab23d5eaac33e69ba21e08023bf18fbafaa9323
Alan Pachuau <alanpachuau@gmail.com>
2014-07-12 21:19:01 +0530

    autocomplete off

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -8,7 +8,7 @@
     <h5><i class="fi-torso"></i> Profile</h5>
     <hr>
 
-    {{ Form::model($user, ['route' => ['users.profile'], 'method' => 'post']) }}
+    {{ Form::model($user, ['route' => ['users.profile'], 'method' => 'post', 'autocomplete' => 'off']) }}
     @include("users/_profile_form")
     {{ Form::close() }}
 
