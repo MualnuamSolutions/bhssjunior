4930b3211c92975a30cb971ef4f16e105071bac1
Alan Pachuau <alanpachuau@gmail.com>
2014-07-14 19:52:20 +0530

    remove tooltip on parent's toolbar

--- app/views/parents/_form.blade.php
+++ app/views/parents/_form.blade.php
@@ -4,12 +4,12 @@
     <div class="small-12 medium-8 columns">
         <div class="row">
             <div class="medium-5 columns">
-                {{ Form::text('regno', Input::get('regno'), ['data-tooltip' => '', 'title' => 'Student registration number', 'placeholder' => 'Registration Number']) }}
+                {{ Form::text('regno', Input::get('regno'), ['title' => 'Student registration number', 'placeholder' => 'Registration Number']) }}
             </div>
             <div class="medium-7 columns">
                 <div class="row collapse">
                     <div class="small-8 medium-9 large-10 columns">
-                        {{ Form::text('contact1', Input::get('contact1'), ['data-tooltip' => '', 'title' => 'This mobile number should be your registered mobile number', 'placeholder' => 'Registered Mobile Number']) }}
+                        {{ Form::text('contact1', Input::get('contact1'), ['title' => 'This mobile number should be your registered mobile number', 'placeholder' => 'Registered Mobile Number']) }}
                     </div>
                     <div class="small-4 medium-3 large-2 columns">
                         {{ Form::button('View', ['class' => 'button postfix', 'type' => 'submit']) }}
