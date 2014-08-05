4ef6c092d643b101c5ec11957342fcecf20c5b3c
Alan Pachuau <alanpachuau@gmail.com>
2014-08-05 19:29:57 +0530

    update environment

---
+++ env.local.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alanpachuau
+ * Date: 05/08/14
+ * Time: 7:10 PM
+ */
+return [
+    'HOST'      =>  '127.0.0.1',
+    'DATABASE'  =>  'bhssjunior',
+    'USERNAME'  =>  'root',
+    'PASSWORD'  =>  'pass'
+];
