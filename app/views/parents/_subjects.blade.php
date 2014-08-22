f748ea3639b5be76a27901d70ed406c338e55e3d
Alan Pachuau <alanpachuau@gmail.com>
2014-08-22 20:08:33 +0530

    update icon

--- app/views/parents/_subjects.blade.php
+++ app/views/parents/_subjects.blade.php
@@ -4,11 +4,11 @@
         <li>
             <a class="test-subject large secondary" href="{{ route('parents.index', ['regno' => $student->regno, 'contact1' => $student->contact1, 'subject' => $subject->id]) }}">
                 <span class="subject-name">{{ $subject->name }}</span>
-                <span class="no-of-test"><i class="fa fa-check"></i><b>{{ Exam::getTests($subject->id, $student->currentClass->id)->count() }}</b> Tests</span>
+                <span class="no-of-test"><i class="fi-check"></i><b>{{ Exam::getTests($subject->id, $student->currentClass->id)->count() }}</b> Tests</span>
                 <span class="teacher-name">
                 <?php $teachers = Exam::getSubjectTeachers($subject->id, $student->currentClass->id); ?>
                     @foreach($teachers as $teacher)
-                    <b><i class="fa fa-user"></i> {{ $teacher->name }}</b>
+                    <b><i class="fi-torso"></i> {{ $teacher->name }}</b>
                     @endforeach
                 </span>
             </a>
